6bb818d9e994d6d809717955f5180fe0334e7381
MurguAdrian <murgu.adrian95@gmail.com>
2026-05-26 20:48:11 +0300

    80%

--- constants/marketingDefaults.ts
+++ constants/marketingDefaults.ts
@@ -2,7 +2,7 @@
 export const SITE_DOMAIN = 'https://www.vibeinvite.ro'
 export const DEFAULT_OG = SITE_DOMAIN + '/social/og-image.png'
 export const OG_WIDTH = 1200
-export const OG_HEIGHT = 630
+export const OG_HEIGHT = 600
 
 export default {
   SITE_DOMAIN,
